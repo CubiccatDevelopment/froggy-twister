3759fd0d20bd933dc5e60827ea17090b3c30d6fd
jkicmal <jakub.kicmal@gmail.com>
2019-05-04 15:52:49 +0200

    initial commit

---
+++ Assets/Bundle.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public static class Bundle
+{
+    public static IEnumerator LerpToRotationRoutine(
+        Transform t, Quaternion rotation, float routineTime,
+        Action onRoutineStart = null, Action onRoutineEnd = null
+        )
+    {
+        onRoutineStart?.Invoke();
+        Quaternion startingRotation = t.rotation;
+        float deltaTime = 0;
+        float f = 0;
+        while (deltaTime < routineTime)
+        {
+            deltaTime += Time.deltaTime;
+            f = deltaTime / routineTime;
+            t.rotation = Quaternion.Lerp(startingRotation, rotation, f);
+            yield return null;
+        }
+        onRoutineEnd?.Invoke();
+    }
+
+    public static IEnumerator LerpToPositionRoutine(
+        Transform t, Vector3 position, float routineTime,
+        Action onRoutineStart = null, Action onRoutineEnd = null)
+    {
+        onRoutineStart?.Invoke();
+        Vector3 startingPosition = t.position;
+        float deltaTime = 0;
+        float f = 0;
+        while (deltaTime < routineTime)
+        {
+            deltaTime += Time.deltaTime;
+            f = deltaTime / routineTime;
+            t.position = Vector3.Lerp(startingPosition, position, f);
+            yield return null;
+        }
+        onRoutineEnd?.Invoke();
+    }
+
+    public static IEnumerator LerpToLocalPositionRoutine(
+        Transform t, Vector3 localPosition, float routineTime,
+        Action onRoutineStart = null, Action onRoutineEnd = null)
+    {
+        onRoutineStart?.Invoke();
+        Vector3 startingPosition = t.localPosition;
+        float deltaTime = 0;
+        float f = 0;
+        while (deltaTime < routineTime)
+        {
+            deltaTime += Time.deltaTime;
+            f = deltaTime / routineTime;
+            t.localPosition = Vector3.Lerp(startingPosition, localPosition, f);
+            yield return null;
+        }
+        onRoutineEnd?.Invoke();
+    }
+
+    public static int GetPreviousIndex(int index, int arrayLength)
+    {
+        int i = index;
+        if (--i < 0)
+            i = arrayLength - 1;
+        return i;
+    }
+
+    public static int GetNextIndex(int index, int arrayLength)
+    {
+        int i = index;
+        if (++i >= arrayLength)
+            i = 0;
+        return i;
+    }
+}
---
+++ Assets/_Game/Sandbox/TEST_WorldCotroller.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_WorldCotroller : MonoBehaviour
+{
+    public bool isGoingDown;
+    public float speed;
+
+    public Transform[] islands;
+
+    public KeyCode setLastIslandPositionKey;
+
+    private int lastIslandIndex = 3;
+
+    bool canAct = true;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(setLastIslandPositionKey) && canAct)
+        {
+            canAct = false;
+            RandomizeLastHeight();
+            RotateOnce();
+            SetLastIslandIndex(Bundle.GetNextIndex(lastIslandIndex, islands.Length));
+        }
+            
+        if(isGoingDown)
+        {
+            foreach (Transform island in islands)
+            {
+                island.position -= Time.deltaTime * speed * Vector3.down;
+            }
+        }
+            
+    }
+
+
+
+    private void RandomizeLastHeight()
+    {
+        Vector3 beforeLastPosition =
+            islands[Bundle.GetPreviousIndex(lastIslandIndex, islands.Length)].localPosition;
+        Vector3 
+            lastPosition = islands[lastIslandIndex].localPosition;
+        lastPosition.y = beforeLastPosition.y + Random.Range(0.1f, 0.5f);
+        StartCoroutine(Bundle.LerpToLocalPositionRoutine(islands[lastIslandIndex], lastPosition, 0.75f));
+    }
+
+    private void SetLastIslandIndex(int index)
+    {
+        lastIslandIndex = index;
+    }
+
+    public void RotateOnce()
+    {
+        Vector3 rotation = transform.eulerAngles;
+        rotation.y = rotation.y + 90f;
+        StartCoroutine(
+            Bundle.LerpToRotationRoutine(transform, Quaternion.Euler(rotation), 0.75f, null, () => canAct = true));
+    }
+
+    
+}
