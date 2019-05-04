ed8fa45d0381be687968545d6ed1f2e2bd19ee4b
jkicmal <jakub.kicmal@gmail.com>
2019-05-04 16:40:31 +0200

    add clouds system prototype

---
+++ Assets/_Game/Scripts/CloudController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CloudController : MonoBehaviour
+{
+    [HideInInspector] public float speed;
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+}
---
+++ Assets/_Game/Scripts/CloudsManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class CloudsManager : MonoBehaviour
+{
+    [SerializeField] Vector2 cloudsSpeeds;
+
+    CloudController[] clouds;
+    BoxCollider boxCollider;
+
+    private void Awake()
+    {
+        clouds = GetComponentsInChildren<CloudController>();
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    private void Start()
+    {
+        foreach (CloudController cloud in clouds)
+            cloud.speed = Random.Range(cloudsSpeeds.x, cloudsSpeeds.y);
+
+        StartCoroutine(UpdateCloudsPositionRoutine());
+    }
+
+    IEnumerator UpdateCloudsPositionRoutine()
+    {
+        yield return new WaitForSeconds(0.25f);
+
+        for (int i = 0; i < clouds.Length; i++)
+        {
+            if(clouds[i].transform.localPosition.x >= boxCollider.size.x / 2)
+            {
+                Vector3 newPosition = clouds[i].transform.localPosition;
+                newPosition.x = -boxCollider.size.x / 2;
+                clouds[i].transform.localPosition = newPosition;
+            }
+        }
+
+        yield return UpdateCloudsPositionRoutine();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(boxCollider == null)
+            boxCollider = GetComponent<BoxCollider>();
+
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawLine(Vector3.left * (boxCollider.size.x / 2), Vector3.right * (boxCollider.size.x / 2));
+    }
+}
